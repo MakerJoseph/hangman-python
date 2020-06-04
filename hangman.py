7c3e4e37844c5b0e3af87e88be9562e87d208ec7
MakerJoseph <1joelmaker@gmail.com>
2020-06-05 01:50:28 +0300

    initial commit

---
+++ hangman.py
@@ -0,0 +1,15 @@
+import random
+from words import word_list
+
+
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+
+
+def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
